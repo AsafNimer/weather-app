ac6f1d3d778cfddda7bda8f2ad6cb0e25bade898
AsafNimer <102157450+AsafNimer@users.noreply.github.com>
2023-08-03 13:01:38 +0200

    current component ui

--- src/components/Current/Current.tsx
+++ src/components/Current/Current.tsx
@@ -44,48 +44,18 @@ function Current(): JSX.Element {
                 </h4>
             </div>
             <div className={styles.component_bottom_container}>
-                <div className={styles.min_max_icon_container}>
-                    <img
-                        className={styles.weather_icon_img}
-                        src={`https://openweathermap.org/img/wn/${currentWeather?.weather[0].icon}@2x.png`}
-                        alt={currentWeather?.weather[0].description}
-                    />
-                    <div className={styles.min_max_container}>
-                        <p className={styles.min}>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                height="1em"
-                                viewBox="0 0 576 512"
-                            >
-                                <path d="M128 112c0-26.5 21.5-48 48-48s48 21.5 48 48V276.5c0 17.3 7.1 31.9 15.3 42.5C249.8 332.6 256 349.5 256 368c0 44.2-35.8 80-80 80s-80-35.8-80-80c0-18.5 6.2-35.4 16.7-48.9c8.2-10.6 15.3-25.2 15.3-42.5V112zM176 0C114.1 0 64 50.1 64 112V276.4c0 .1-.1 .3-.2 .6c-.2 .6-.8 1.6-1.7 2.8C43.2 304.2 32 334.8 32 368c0 79.5 64.5 144 144 144s144-64.5 144-144c0-33.2-11.2-63.8-30.1-88.1c-.9-1.2-1.5-2.2-1.7-2.8c-.1-.3-.2-.5-.2-.6V112C288 50.1 237.9 0 176 0zm0 416c26.5 0 48-21.5 48-48c0-20.9-13.4-38.7-32-45.3V272c0-8.8-7.2-16-16-16s-16 7.2-16 16v50.7c-18.6 6.6-32 24.4-32 45.3c0 26.5 21.5 48 48 48zm336-64H480V64c0-17.7-14.3-32-32-32s-32 14.3-32 32V352H384c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l64 64c6 6 14.1 9.4 22.6 9.4s16.6-3.4 22.6-9.4l64-64c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8z" />
-                            </svg>
-                            {currentWeather?.main.temp_min !== undefined
-                                ? `Min: ${Math.floor(
-                                      currentWeather?.main.temp_min
-                                  )}° `
-                                : ""}
-                        </p>
-                        <p>|</p>
-                        <p className={styles.max}>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                height="1em"
-                                viewBox="0 0 576 512"
-                            >
-                                <path d="M128 112c0-26.5 21.5-48 48-48s48 21.5 48 48V276.5c0 17.3 7.1 31.9 15.3 42.5C249.8 332.6 256 349.5 256 368c0 44.2-35.8 80-80 80s-80-35.8-80-80c0-18.5 6.2-35.4 16.7-48.9c8.2-10.6 15.3-25.2 15.3-42.5V112zM176 0C114.1 0 64 50.1 64 112V276.4c0 .1-.1 .3-.2 .6c-.2 .6-.8 1.6-1.7 2.8C43.2 304.2 32 334.8 32 368c0 79.5 64.5 144 144 144s144-64.5 144-144c0-33.2-11.2-63.8-30.1-88.1c-.9-1.2-1.5-2.2-1.7-2.8c-.1-.3-.2-.5-.2-.6V112C288 50.1 237.9 0 176 0zm0 416c26.5 0 48-21.5 48-48c0-20.9-13.4-38.7-32-45.3V112c0-8.8-7.2-16-16-16s-16 7.2-16 16V322.7c-18.6 6.6-32 24.4-32 45.3c0 26.5 21.5 48 48 48zM480 160h32c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-64-64c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8h32V448c0 17.7 14.3 32 32 32s32-14.3 32-32V160z" />
-                            </svg>
-                            {currentWeather?.main.temp_max !== undefined
-                                ? `Max: ${Math.ceil(
-                                      currentWeather?.main.temp_max
-                                  )}°`
-                                : ""}
-                        </p>
+                <div className={styles.fls_like_hum_wnd_poll_icon_container}>
+                    <div className={styles.img_div}>
+                        <img
+                            className={styles.weather_icon_img}
+                            src={`https://openweathermap.org/img/wn/${currentWeather?.weather[0].icon}@2x.png`}
+                            alt={currentWeather?.weather[0].description}
+                        />
                     </div>
-                </div>
-                <div className={styles.fls_like_hum_wnd_poll_container}>
                     <div className={styles.data_wrapper}>
                         <p className={styles.feels_like}>
                             <svg
+                                className={styles.data_icon}
                                 xmlns="http://www.w3.org/2000/svg"
                                 height="1em"
                                 viewBox="0 0 320 512"
@@ -100,6 +70,7 @@ function Current(): JSX.Element {
                         </p>
                         <p className={styles.humidity}>
                             <svg
+                                className={styles.data_icon}
                                 xmlns="http://www.w3.org/2000/svg"
                                 height="1em"
                                 viewBox="0 0 384 512"
@@ -114,6 +85,7 @@ function Current(): JSX.Element {
                         </p>
                         <p className={styles.wind}>
                             <svg
+                                className={styles.data_icon}
                                 xmlns="http://www.w3.org/2000/svg"
                                 height="1em"
                                 viewBox="0 0 512 512"
@@ -128,6 +100,7 @@ function Current(): JSX.Element {
                         </p>
                         <p className={styles.pollution}>
                             <svg
+                                className={styles.data_icon}
                                 xmlns="http://www.w3.org/2000/svg"
                                 height="1em"
                                 viewBox="0 0 640 512"
@@ -143,11 +116,43 @@ function Current(): JSX.Element {
                                 : ""}
                         </p>
                     </div>
-                    <div className={styles.sunrise_sunset_container}>
-                        <p className={styles.sunrise}>sunrise</p>
-                        <p>|</p>
-                        <p className={styles.sunset}>sunset</p>
-                    </div>
+                </div>
+                <div className={styles.minmax_sunrise_sunset_container}>
+                    <p className={styles.min}>
+                        <svg
+                            className={styles.data_icon}
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="1em"
+                            viewBox="0 0 576 512"
+                        >
+                            <path d="M128 112c0-26.5 21.5-48 48-48s48 21.5 48 48V276.5c0 17.3 7.1 31.9 15.3 42.5C249.8 332.6 256 349.5 256 368c0 44.2-35.8 80-80 80s-80-35.8-80-80c0-18.5 6.2-35.4 16.7-48.9c8.2-10.6 15.3-25.2 15.3-42.5V112zM176 0C114.1 0 64 50.1 64 112V276.4c0 .1-.1 .3-.2 .6c-.2 .6-.8 1.6-1.7 2.8C43.2 304.2 32 334.8 32 368c0 79.5 64.5 144 144 144s144-64.5 144-144c0-33.2-11.2-63.8-30.1-88.1c-.9-1.2-1.5-2.2-1.7-2.8c-.1-.3-.2-.5-.2-.6V112C288 50.1 237.9 0 176 0zm0 416c26.5 0 48-21.5 48-48c0-20.9-13.4-38.7-32-45.3V272c0-8.8-7.2-16-16-16s-16 7.2-16 16v50.7c-18.6 6.6-32 24.4-32 45.3c0 26.5 21.5 48 48 48zm336-64H480V64c0-17.7-14.3-32-32-32s-32 14.3-32 32V352H384c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l64 64c6 6 14.1 9.4 22.6 9.4s16.6-3.4 22.6-9.4l64-64c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8z" />
+                        </svg>
+                        {currentWeather?.main.temp_min !== undefined
+                            ? `Min: ${Math.floor(
+                                  currentWeather?.main.temp_min
+                              )}° `
+                            : ""}
+                    </p>
+                    <p className={styles.vertical_line_par}>|</p>
+                    <p className={styles.max}>
+                        <svg
+                            className={styles.data_icon}
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="1em"
+                            viewBox="0 0 576 512"
+                        >
+                            <path d="M128 112c0-26.5 21.5-48 48-48s48 21.5 48 48V276.5c0 17.3 7.1 31.9 15.3 42.5C249.8 332.6 256 349.5 256 368c0 44.2-35.8 80-80 80s-80-35.8-80-80c0-18.5 6.2-35.4 16.7-48.9c8.2-10.6 15.3-25.2 15.3-42.5V112zM176 0C114.1 0 64 50.1 64 112V276.4c0 .1-.1 .3-.2 .6c-.2 .6-.8 1.6-1.7 2.8C43.2 304.2 32 334.8 32 368c0 79.5 64.5 144 144 144s144-64.5 144-144c0-33.2-11.2-63.8-30.1-88.1c-.9-1.2-1.5-2.2-1.7-2.8c-.1-.3-.2-.5-.2-.6V112C288 50.1 237.9 0 176 0zm0 416c26.5 0 48-21.5 48-48c0-20.9-13.4-38.7-32-45.3V112c0-8.8-7.2-16-16-16s-16 7.2-16 16V322.7c-18.6 6.6-32 24.4-32 45.3c0 26.5 21.5 48 48 48zM480 160h32c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-64-64c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8h32V448c0 17.7 14.3 32 32 32s32-14.3 32-32V160z" />
+                        </svg>
+                        {currentWeather?.main.temp_max !== undefined
+                            ? `Max: ${Math.ceil(
+                                  currentWeather?.main.temp_max
+                              )}°`
+                            : ""}
+                    </p>
+                    <p className={styles.vertical_line_par}>|</p>
+                    <p className={styles.sunrise}>sunrise</p>
+                    <p className={styles.vertical_line_par}>|</p>
+                    <p className={styles.sunset}>sunset</p>
                 </div>
             </div>
         </div>
