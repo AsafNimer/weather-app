5cb5ceb58737daf7e41040e30df78ac919aff4df
AsafNimer <102157450+AsafNimer@users.noreply.github.com>
2023-08-21 20:07:32 +0200

    fixing map is undefined

--- src/App.tsx
+++ src/App.tsx
@@ -140,28 +140,30 @@ function App(): JSX.Element {
                     </div>
                 </div>
                 <ul className={styles.cities_list}>
-                    {searchResults.map(
-                        (result: FirstApiResultType, index: number) => (
-                            <li
-                                key={`${result.name}-${index}`}
-                                onClick={() => handleCitySelect(result)}
-                                className={styles.city_item}
-                            >
-                                <button className={styles.city_btn}>
-                                    <svg
-                                        className={styles.location_icon}
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        height="1em"
-                                        viewBox="0 0 384 512"
-                                    >
-                                        <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
-                                    </svg>
-                                    {result.name}, {result.country}{" "}
-                                    {result.state ? result.state : ""}
-                                </button>
-                            </li>
-                        )
-                    )}
+                    {searchResults
+                        ? searchResults.map(
+                              (result: FirstApiResultType, index: number) => (
+                                  <li
+                                      key={`${result.name}-${index}`}
+                                      onClick={() => handleCitySelect(result)}
+                                      className={styles.city_item}
+                                  >
+                                      <button className={styles.city_btn}>
+                                          <svg
+                                              className={styles.location_icon}
+                                              xmlns="http://www.w3.org/2000/svg"
+                                              height="1em"
+                                              viewBox="0 0 384 512"
+                                          >
+                                              <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
+                                          </svg>
+                                          {result.name}, {result.country}{" "}
+                                          {result.state ? result.state : ""}
+                                      </button>
+                                  </li>
+                              )
+                          )
+                        : []}
                 </ul>
             </div>
             <ForecastContext.Provider
